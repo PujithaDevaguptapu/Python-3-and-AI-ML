cedef4f91073dd1ee7c8a2ef6988682c59778963
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2022-11-11 22:02:02 +0530

    Create Block Letters.py

---
+++ Block Letters.py
@@ -0,0 +1,28 @@
+#My first name is Pujitha and my last name is Devaguptapu ne Vissamraju
+# I love to organize home and be in command of things. I also admired Twilight.\
+print('P P P P')
+print('P      P')
+print('P      P')
+print('P P P P')
+print('P')
+print('P')
+print('P')
+last_name = """
+D D D D 
+D       D
+D       D
+D       D
+D       D
+D       D
+D D D D """
+print(last_name)
+
+snowman = """
+    _________
+   |         |
+   _ ________ _
+    (*     *)
+   (    :     )
+  (     :      )
+"""
+print(snowman)
