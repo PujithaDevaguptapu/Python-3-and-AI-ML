d4a7fc2a205e746d9c13b6c35a03da144cd87124
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2022-11-22 01:23:10 +0530

    Create Large Power.py

---
+++ Large Power.py
@@ -0,0 +1,11 @@
+# Write your large_power function here:
+def large_power(base, exponent):
+  if base ** exponent > 5000:
+    return True
+  else:
+    return False
+#Uncomment these function calls to test your large_power function:
+print(large_power(2, 13))
+# should print True
+print(large_power(2, 12))
+# should print False
