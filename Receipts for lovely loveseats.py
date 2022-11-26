10940ce5834907239878ce4e152ed6b52c329eb7
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2022-11-26 23:03:31 +0530

    Update Receipts for lovely loveseats.py

--- Receipts for lovely loveseats.py
+++ Receipts for lovely loveseats.py
@@ -1,10 +1,19 @@
+#Description and price for loveseat
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 lovely_loveseat_price = 254.00
+
+#Description and price of stylish settee
 stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
 stylish_settee_price = 180.50
+
+#Description and price of luxurious lamp
 luxurious_lamp_description = "Luxurious Lamp. glass and iron. 36 inches tall. Brown with cream shade."
 luxurious_lamp_price = 52.15
+
+
 sales_tax = .088
+
+#First customer purchase
 customer_one_total = 0
 customer_one_itemization = ""
 customer_one_total = customer_one_total + lovely_loveseat_price
@@ -13,6 +22,8 @@ customer_one_total += luxurious_lamp_price
 customer_one_itemization += luxurious_lamp_description
 customer_one_tax = customer_one_total * sales_tax
 customer_one_total += customer_one_tax
+
+
 print("Customer One Items:")
 print(customer_one_itemization)
 print("Customer One Total:")
