7ebd5fce8fff8b783e30a86a421bfa9cd56e10e8
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2022-12-19 22:11:22 +0530

    Create String Methods - Formatting Methods.py

---
+++ String Methods - Formatting Methods.py
@@ -0,0 +1,12 @@
+poem_title = "spring storm"
+poem_author = "William Carlos Williams"
+
+#Using lower() string method
+poem_title_fixed  = poem_title.title()
+print(poem_title)
+print(poem_title_fixed)
+
+#Using upper() string method
+poem_author_fixed = poem_author.upper()
+print(poem_author)
+print(poem_author_fixed)
