c051a4e64bce9f831a58ab62a22925e8c58e3d6d
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-01-16 22:41:42 +0530

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,4 +1,14 @@
+# Dictionary creation
 ensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
 num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
 
 print(sensors)
+
+
+#Adding key and values to a dictoonary
+animals_in_zoo = {}
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+
+print(animals_in_zoo)
