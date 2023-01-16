f348a3ecb3aad3ed6827d784b55f4aa8462ff9d8
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-01-16 23:01:21 +0530

    Update dict.py

--- dict.py
+++ dict.py
@@ -12,3 +12,17 @@ animals_in_zoo["monkeys"] = 12
 animals_in_zoo["dinosaurs"] = 0
 
 print(animals_in_zoo)
+
+
+#Adding multiple keys
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print(user_ids)
+
+#Dictionary Comprehension
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+
+zipped_drinks = zip(drinks, caffeine)
+#Dict Comprehension in this example
+drinks_to_caffeine = {drinks:caffeine for drinks, caffeine in zipped_drinks}
