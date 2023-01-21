14cdc614f34009fff859b57998309cf4f2d44014
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-01-22 00:01:27 +0530

    Create Writing a json file.py

---
+++ Writing a json file.py
@@ -0,0 +1,10 @@
+data_payload = [
+  {'interesting message': 'What is JSON? A web application\'s little pile of secrets.',
+   'follow up': 'But enough talk!'}
+]
+
+import json
+
+#writing json file
+with open('data.json', 'w') as data_json:
+  json.dump(data_payload, data_json)
