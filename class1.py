63c97bded5bbb8b96a8ae9d4a5bcd2897f1d045f
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-01-23 23:27:56 +0530

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,34 @@
+#Types
+print(type(5))
+
+my_dict = {}
+print(type(my_dict))
+
+my_list = []
+print(type(my_list))
+
+#Class
+class Facade:
+  pass
+
+#Instantiation
+class Facade:
+  pass
+facade_1 = Facade()
+
+#OOP
+class Facade:
+  pass
+
+facade_1 = Facade()
+facade_1_type = type(facade_1)
+print(facade_1_type)
+
+#Class Variables
+class Grade:
+    minimum_passing = 65
+    
+#Methods
+class Rules:
+  def washing_brushes(self):
+    return "Point bristles towards the basin while washing your brushes."
