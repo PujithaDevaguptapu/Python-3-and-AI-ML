4f7c663d3b43f74d8e4b365b094cc7eed3334d89
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-02-22 19:26:58 +0530

    Create Negative Indices.py

---
+++ Negative Indices.py
@@ -0,0 +1,9 @@
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+#Using Negative Indices
+second_to_last = company_motto[-2]
+print(second_to_last)
+
+#Slicing using negative indices
+final_word = company_motto[-4:]
+print(final_word)
