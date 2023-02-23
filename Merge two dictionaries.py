a0aecb0eaf40145ebffad3955f38b0c171d8ac26
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-02-23 12:03:28 +0530

    Create Merge two dictionaries.py

---
+++ Merge two dictionaries.py
@@ -0,0 +1,20 @@
+#Using | operator
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print(dict_1 | dict_2)
+
+#Using ** operator to unpack dictionaries and the dictionaries are merged by placing them inside {}
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print({**dict_1, **dict_2})
+
+#Using copy() and update() method
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+dict_3 = dict_2.copy()
+dict_3.update(dict_1)
+
+print(dict_3)
