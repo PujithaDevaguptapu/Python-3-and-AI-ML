7c975c65bfedfdf9871cf51210e11a249f14420a
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-02-26 19:50:20 +0530

    Create Delete a key.py

---
+++ Delete a key.py
@@ -0,0 +1,11 @@
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+
+#In one-line writting the value of variables and deleting them with a default value if they key does not exist
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("power stew", 0)
+health_points += available_items.pop("mystic bread", 0)
+
+print(available_items)
+print(health_points)
